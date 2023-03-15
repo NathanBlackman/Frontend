cebe5d9cbe212ddc64498ed8a3436e0ee53203f2
Nathan Blackman <nathanpgblackman@gmail.com>
2023-03-15 12:48:06 -0500

    navbar starting to take form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navigation from './Components/Navigation';
 import Router from './Router/Index';
 
 function App() {
   return (
     <div>
+      <Navigation />
       <Router />
     </div>
   );
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,7 +3,20 @@ import React from 'react';
 export default function Navigation() {
     return (
         <div>
-            <div className="navigation"></div>
+            <div className="navigation">
+                <img
+                    className='playgroundlogo'
+                    alt='playgroundlogo'
+                    src='https://media.istockphoto.com/id/1272460291/vector/swing-icon-swings-vector.jpg?s=612x612&w=0&k=20&c=cinVrg6KIt_cW3_sJTDi0b3wnUPtaY0C2slogF4yGYU='
+                />
+                <div className='navbarOptions'>
+                    
+                    <div className='option1'>
+                        <h1>Home</h1>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
